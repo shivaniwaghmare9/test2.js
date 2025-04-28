20244da51039aa5be37712db59a19117eb3c4452
shivani <shivaniw69@gmail.com>
2025-04-28 08:09:45 +0530

    test

--- test.js/que5.js
+++ test.js/que5.js
@@ -1,32 +1,47 @@
 
 
 
-// function shiv(){
-//   let ans=document.querySelector("#btn")
-//   if (ans===0){
-//     ans.innerHTML=
-//   }
-// }
+function shiv(){
+  let ans=document.querySelector("#btn1")
+  let ans1=document.querySelector("#btn2")
+  let ans2=document.querySelector("#btn3")
 
- let shiv=()=>{
-let aoo=[
-  {
-    name:"shivani",
-    age:23,
-    city:"bhopal"
-  },{
-    name:"shii",
-    age:24,
-    city:"indore"
-  },{
-    name:"shiv",
-    age:25,
-    city:"pune"
-  }
-]
+  let show1=Math.floor(Math.random()*6)+1;
+  let show2=Math.floor(Math.random()*6)+1;
+  let show3=Math.floor(Math.random()*6)+1;
 
-  let ans=aoo.map((e)=>{
-    return console.log(e.ans);
-
-  })
+  ans.innerHTML=show1;
+  ans1.innerHTML=show2;
+  ans2.innerHTML=show3;
+  
 }
+
+ 
+// let aoo=[
+//   {
+//     name:"shivani",
+//     age:23,
+//     city:"bhopal"
+//   },{
+//     name:"shii",
+//     age:24,
+//     city:"indore"
+//   },{
+//     name:"shiv",
+//     age:25,
+//     city:"pune"
+//   }
+// ]
+//  let show1=document.querySelector("#show");
+ 
+//   aoo.map((e)=>{
+//     show1.innerHTML+=
+//     `
+//     <tr>
+//       <td>${e.name}</td>
+//       <td>${e.age}</td>
+//       <td>${e.city}</td>
+//     </tr>
+//     `
+
+//   })
