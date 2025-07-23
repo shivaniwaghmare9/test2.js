94d9d840866865a19337843e94f6c395a64f88fa
shivani <shivaniw69@gmail.com>
2025-07-23 15:10:54 +0530

    layout

--- watch5pm-master/src/Layout.jsx
+++ watch5pm-master/src/Layout.jsx
@@ -1,3 +1,22 @@
+// import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import TopMenu from "./components/TopMenu";
+// import { Outlet } from "react-router-dom";
+
+// const Layout=()=>{
+//     return(
+//         <>
+//         <div id="topHeader"> 
+//         <Header/>
+//         <TopMenu/>
+//         </div>
+//          <Outlet/>
+//           <Footer/>
+//         </>
+//     )
+// }
+
+// export default Layout;
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import TopMenu from "./components/TopMenu";
@@ -10,11 +29,8 @@ const Layout=()=>{
         <Header/>
         <TopMenu/>
         </div>
-         
-             <Outlet/>
+         <Outlet/>
           <Footer/>
-          
-
         </>
     )
 }
