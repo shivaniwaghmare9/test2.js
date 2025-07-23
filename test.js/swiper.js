c80cd31a630faef2815ec87c3813282e327d2ee0
shivani <shivaniw69@gmail.com>
2025-07-23 15:07:50 +0530

    swiper

--- test.js/swiper.js
+++ test.js/swiper.js
@@ -1,15 +1,4 @@
 
-// let swiper=new Swiper(".swiper",{
-//     navigation:{
-//         nextE1:".swiper-button-next",
-//         prevE1:".swiper-button-prev"
-//     },
-//     autoplay:{
-//         delay:2000,
-//     }
-   
-// })
-
 let swiper=new Swiper(".swiper",{
     navigation:{
         nextE1:".swiper-button-next",
