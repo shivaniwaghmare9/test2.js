f6fce6b642ae745e3d24c271c47e98a2ab277331
shivani <shivaniw69@gmail.com>
2025-07-23 15:06:29 +0530

    js5

--- test.js/que5.js
+++ test.js/que5.js
@@ -1,6 +1,54 @@
 
 
 
+// function shiv(){
+//   let ans=document.querySelector("#btn1")
+//   let ans1=document.querySelector("#btn2")
+//   let ans2=document.querySelector("#btn3")
+
+//   let show1=Math.floor(Math.random()*6)+1;
+//   let show2=Math.floor(Math.random()*6)+1;
+//   let show3=Math.floor(Math.random()*6)+1;
+
+//   ans.innerHTML=show1;
+//   ans1.innerHTML=show2;
+//   ans2.innerHTML=show3;
+  
+// }
+
+ 
+// // let aoo=[
+// //   {
+// //     name:"shivani",
+// //     age:23,
+// //     city:"bhopal"
+// //   },{
+// //     name:"shii",
+// //     age:24,
+// //     city:"indore"
+// //   },{
+// //     name:"shiv",
+// //     age:25,
+// //     city:"pune"
+// //   }
+// // ]
+// //  let show1=document.querySelector("#show");
+ 
+// //   aoo.map((e)=>{
+// //     show1.innerHTML+=
+// //     `
+// //     <tr>
+// //       <td>${e.name}</td>
+// //       <td>${e.age}</td>
+// //       <td>${e.city}</td>
+// //     </tr>
+// //     `
+
+// //   })
+
+
+
+
 function shiv(){
   let ans=document.querySelector("#btn1")
   let ans1=document.querySelector("#btn2")
@@ -45,3 +93,4 @@ function shiv(){
 //     `
 
 //   })
+
