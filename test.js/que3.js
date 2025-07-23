5191ca784180391ea5a60a8ab9c96c4c7ea6c0a1
shivani <shivaniw69@gmail.com>
2025-07-23 15:04:55 +0530

    js3

--- test.js/que3.js
+++ test.js/que3.js
@@ -1,25 +1,4 @@
 
-// let shiv=()=>{
-
-// let inpname= document.querySelector("#name").value
-// let inpemail= document.querySelector("#email").value
-
-// let errname=document.querySelector("#errname")
-// let erremail=document.querySelector("#erremail")
-
-// if(inpname==""){
-//     errname.innerHTML="please enter the name"
-//     errname.style.color="red"
-//     return false;
-// }
-
-//  else if(!(inpemail.includes('@')&& inpemail.includes(".com"))){
-//     erremail.innerHTML="please enter valid email"
-//     erremail.style.color="red"
-//     return false;
-// }
-// }
-
 let shiv=()=>{
 
 let inpname= document.querySelector("#name").value
@@ -40,4 +19,3 @@ if(inpname==""){
     return false;
 }
 }
-
