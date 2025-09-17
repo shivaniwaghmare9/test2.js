490cea802810b0941098d36692cab969ccc285ff
shivani <shivaniw69@gmail.com>
2025-09-17 23:11:25 +0530

    header

--- watch5pm-master/src/components/Header.jsx
+++ watch5pm-master/src/components/Header.jsx
@@ -1,3 +1,22 @@
+// import { FaShoppingCart } from "react-icons/fa";
+// import { useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+
+// const Header=()=>{
+// const cartData = useSelector(state=>state.mycart.cart);
+// const cartLength= cartData.length;
+// const navigate= useNavigate();
+//   return(
+//         <>
+//           <div id="header"> 
+//             {cartLength}
+//           <FaShoppingCart  onClick={()=>{navigate("/mycart")}}  style={{cursor:"pointer"}}/>
+//           </div>
+//         </>
+//     )
+// }
+
+// export default Header;
 import { FaShoppingCart } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
