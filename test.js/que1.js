46e995b707d8e16fad9c3b729211995261c28957
shivani <shivaniw69@gmail.com>
2025-10-11 22:35:21 +0530

    js

--- test.js/que1.js
+++ test.js/que1.js
@@ -1,4 +1,19 @@
 
+//  function shiv(){
+//   let ans=prompt("enter the number")
+//     if(ans>0){
+        
+//         alert("positive")
+//     }
+//     else if(ans<0){
+//        alert("negative")
+
+//     }
+//      else {
+//         alert("zero")
+//     }
+// }   
+
  function shiv(){
   let ans=prompt("enter the number")
     if(ans>0){
