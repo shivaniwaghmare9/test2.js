916a57bb34490a6b4419ad6f6004d67af86a31c2
shivani <shivaniw69@gmail.com>
2025-10-11 22:37:06 +0530

    que2

--- test.js/que2.js
+++ test.js/que2.js
@@ -1,5 +1,19 @@
 
 
+// let ans={
+    
+//     name:"shivani",
+//     age:23,
+//     greet:function(){
+//         console.log(`Hello my name is ${this.name} and i am ${this.age} years old`);
+//     }
+// }
+// ans.greet()
+
+
+
+
+
 let ans={
     
     name:"shivani",
